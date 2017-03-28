911048d1401c2365dfacd54c7397d91400acf880
cemre.mengu <cemre.mengu@ttgint.com>
2017-03-28 15:19:25 +0300

    Initial, subscription observable

---
+++ RawRabbit.Reactive/src/RawRabbit.Reactive/RawRabbitReactiveExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace RawRabbit.Reactive
+{
+    using System;
+
+    public static class RawRabbitReactiveExtensions
+    {
+        public static IObservable<T> CreateSubscriptionObservable<T>(this IBusClient busClient) where T : class
+        {
+            return new RawRabbitSubscriptionObservable<T>(busClient);
+        }
+    }
+}
---
+++ RawRabbit.Reactive/src/RawRabbit.Reactive/RawRabbitSubscriptionObservable.cs
@@ -0,0 +1,33 @@
+﻿namespace RawRabbit.Reactive
+{
+    using System;
+    using System.Reactive.Disposables;
+    using System.Threading.Tasks;
+
+    public class RawRabbitSubscriptionObservable<T> : IObservable<T> where T : class
+    {
+        private readonly IBusClient _busClient;
+        private IObserver<T> _observer;
+
+        protected internal RawRabbitSubscriptionObservable(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            _observer = observer;
+
+            try
+            {
+                _busClient.SubscribeAsync<T>(message => { return Task.Run(() => _observer.OnNext(message)); });
+            }
+            catch (Exception e)
+            {
+                _observer.OnError(e);
+            }
+
+            return Disposable.Empty;
+        }
+    }
+}
